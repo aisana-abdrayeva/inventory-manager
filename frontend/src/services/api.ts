da37b6fe2efdabe40bcc10fd307e8efc28acf2dd
Aisana Abdrayeva <aisana.abdrayeva@gmail.com>
2025-08-15 00:20:30 +0500

    server

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,5 +4,5 @@ const api = axios.create({
     baseURL: "inventory-manager-backend-production-7a87.up.railway.app",
     withCredentials: true, 
 });
-
+//inventory-manager-backend-production-7a87.up.railway.app
 export default api;
