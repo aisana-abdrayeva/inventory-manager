3e5057102217c138a836cabfbf6894b0bd5f5e6e
Aisana Abdrayeva <aisana.abdrayeva@gmail.com>
2025-08-14 17:19:38 +0500

    back and front connect

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "inventory-manager-backend-production-7a87.up.railway.app/api",
     withCredentials: true, 
 });
 
