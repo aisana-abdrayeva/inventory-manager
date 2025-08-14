5344f8a58abb7c7bd25f8c24877d6b353533cd1d
Aisana Abdrayeva <aisana.abdrayeva@gmail.com>
2025-08-14 23:18:39 +0500

    server update again

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,13 +8,13 @@ const passport = require('passport');
 dotenv.config();
 
 const app = express();
-// app.use(cors(
-//     {
-//         origin: ["https://inventory-manager-production-7ab5.up.railway.app/"],
-//         credentials: true,
-//     }
-// ));
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://inventory-manager-production-7ab5.up.railway.app/"],
+        credentials: true,
+    }
+));
+
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", require("./routes/auth")); 
 // app.use("/api/social-auth", require("./routes/socialAuth"));
-// app.use("/api/users", require("./routes/users")); 
+app.use("/api/users", require("./routes/users")); 
 
 app.listen(5000, () => {
     console.log(`Server running on port 5000`);
