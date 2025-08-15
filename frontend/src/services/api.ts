57d39da97aa3d7c18abb66e84fd3ce33445583be
Aisana Abdrayeva <aisana.abdrayeva@gmail.com>
2025-08-16 00:24:28 +0500

    link change

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "inventory-manager-backend-production-7a87.up.railway.app",
+    baseURL: "https://inventory-manager-backend-production-7a87.up.railway.app",
     withCredentials: true, 
 });
 
