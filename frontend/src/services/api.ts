66bd7e4ca7e4d42d7c9931ae27ba32a37475d375
Aisana Abdrayeva <aisana.abdrayeva@gmail.com>
2025-08-16 02:58:26 +0500

    cors

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://inventory-manager-backend-production-7a87.up.railway.app", 
-    withCredentials: false, 
+    withCredentials: true, 
 });
 
 export default api;
